3e6cd4c906f4d4bbe615ca3017daa1414e9b9e95
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-07-15 02:04:44 -0300

    try fix preview.js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,9 +1,16 @@
+const { mergeConfig } = require('vite');
+
 module.exports = {
   "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
   "features": {
     "storyStoreV7": true
@@ -14,7 +21,7 @@ module.exports = {
     if (configType === 'PRODUCTION') {
       config.base = '/design-system-storybook/';
     }
-    return config;
+    return mergeConfig(config);
   },
   docs: {
     autodocs: true
