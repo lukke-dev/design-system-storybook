f75c4e1ef9ad34fa6fd6d72396bcd736c6387f0b
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-07-15 01:51:14 -0300

    🐳 chore(Update storybook config to V2):

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,26 +1,22 @@
 module.exports = {
-  "stories": [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, { configType }) => {
+  viteFinal: (config, {
+    configType
+  }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/design-system-storybook/'
+      config.base = '/design-system-storybook/';
     }
-
-    return config
+    return config;
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
